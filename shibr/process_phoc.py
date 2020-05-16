4817745fa5f0548f049eebc18bf975ddba2ca2a5
L <nope>
2020-05-16 09:54:56 +0200

    Some scripting improvements

--- shibr/process_phoc.py
+++ shibr/process_phoc.py
@@ -1,10 +1,17 @@
 import numpy as np
 
-def hits(arr):
-    for i in range(len(arr)):
-        if arr[i] > 0.7:
-            print(i)
-
-arr = np.load('predicted_phocs.py')
+word = 'anna'
+arr = np.load('predicted_phocs.npy')
 sliced = np.concatenate((arr[0][:36], arr[0][86:122], arr[0][172:]))
-hits(sliced)
+normalized = np.around(sliced, 0)
+truth = np.load(word + '.npy')[0]
+masked = np.ma.masked_where(truth==0, normalized)
+print("Result for " + word)
+masked.sum()
+
+# svens: 0
+# swen: 0
+# anders: 1 (25)
+# walfrid: 3 (28)
+# maria: 2 (22)
+# anna: 1 (15)
